5a5e7ff9efcfe698c824d0fc1462185a6ae41aeb
6y6a <gubinalexeymain@gmail.com>
2017-09-03 12:44:33 +0300

    Подгрузка контента

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 require(["game"], function(Game) {
   function App() {
     Game.apply(this, arguments);
-    canvas.width = 200;
-    canvas.height = 100;
-    canvas.scale = 2;
+    canvas.width = 480;
+    canvas.height = 272;
+    canvas.scale = 1;
 
+    content.load('back', 'img/back.png');
+    content.load('blocks', 'img/blocks.png');
+    content.load('numbers', 'img/numbers.png');
   }
 
+
   App.prototype = Object.create(Game.prototype);
+
+
+  App.prototype.tick = function() {
+    if (content.progress() === 1) {
+      canvas.ctx.drawImage(content.get('back'), 0, 0);
+    }
+  };
 
 
 
--- js/game.js
+++ js/game.js
@@ -1,11 +1,23 @@
 define(function () {
 
   function Game() {
-
+    this._reqframe;
   }
 
+  Game.prototype.tick = function () {
+    //console.log('перезаписать');
+
+  };
+
   Game.prototype.run = function () {
-    console.log('running');
+
+    var self = this;
+    function loop() {
+      self._reqframe = window.requestAnimationFrame(loop);
+      self.tick();
+      canvas.flip();
+    }
+    this._reqframe = window.requestAnimationFrame(loop);
   };
 
   return Game;

4f21d5d2a50a3830fe3aea301863f47f244b19a0
6y6a <gubinalexeymain@gmail.com>
2017-09-03 14:21:32 +0300

    Добавление статистик

--- js/tetris.js
+++ js/tetris.js
@@ -5,8 +5,26 @@ define(['numfont'], function (Numfont) {
 
     var num = content.get('numbers');
     this.font = {
-      gray: new Numfont(num, 0, 9)
-  }
+      gray  : new Numfont(num,  0, 9),
+      cyan  : new Numfont(num,  9, 9),
+      red   : new Numfont(num, 18, 9),
+      blue  : new Numfont(num, 27, 9),
+      orange: new Numfont(num, 36, 9),
+      green : new Numfont(num, 45, 9),
+      yellow: new Numfont(num, 54, 9),
+      purple: new Numfont(num, 63, 9)
+    };
+
+    this.data = {
+      L: 0,
+      I: 0,
+      T: 0,
+      S: 0,
+      Z: 0,
+      O: 0,
+      J: 0,
+      Total: 0
+    }
   }
 
   Tetris.prototype.handleInputs = function (input) {
@@ -18,7 +36,16 @@ define(['numfont'], function (Numfont) {
 
   Tetris.prototype.draw = function (ctx) {
     ctx.drawImage(this.back, 0 ,0);
-    this.font.gray.draw(ctx, 42, 10, 10, 8);
+
+    this.font.orange.draw(ctx, this.data.L, 432,  52, 5);
+    this.font.cyan.draw(  ctx, this.data.I, 432,  76, 5);
+    this.font.purple.draw(ctx, this.data.T, 432, 100, 5);
+    this.font.green.draw( ctx, this.data.S, 432, 124, 5);
+    this.font.red.draw(   ctx, this.data.Z, 432, 148, 5);
+    this.font.yellow.draw(ctx, this.data.O, 432, 172, 5);
+    this.font.blue.draw(  ctx, this.data.J, 432, 196, 5);
+
+    this.font.gray.draw(ctx, this.data.Total, 425, 221, 6);
   };
 
   return Tetris;
